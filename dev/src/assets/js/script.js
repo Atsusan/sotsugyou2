1e1c563eace0962837e65e26d233593b705a2043
Atsusan <as2k_abc@hotmail.com>
2023-08-21 14:52:23 +0900

    mv追加

--- dev/src/assets/js/script.js
+++ dev/src/assets/js/script.js
@@ -14,3 +14,20 @@ const swiper = new Swiper(".swiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+// ハンバーガーメニュー実装
+
+jQuery(".js-openbtn").click(function () {
+    jQuery(this).toggleClass("active"); // ボタン自身にactiveクラスを付与
+    jQuery("#js-g-nav").toggleClass("panelactive"); //ナビゲーションにpanelactiveクラスを付与
+});
+
+jQuery("#js-g-nav a").click(function () {
+    jQuery(".js-openbtn").removeClass("active"); //ボタンのactiveクラスを削除
+    jQuery("#js-g-nav").removeClass("panelactive"); //ナビゲーションのpanelactiveクラスを削除
+});
+
+jQuery("#js-g-nav").click(function () {
+    // 背面をクリックしたら解除する処置
+    jQuery(".js-openbtn").removeClass("active");
+    jQuery("#js-g-nav").removeClass("panelactive");
+});
